8c281a7a018e232779250b91f45c00935015c0c4
Martin Sivak <xsivakm1@stuba.sk>
2024-05-04 19:47:47 +0200

    Create basic layouts and partials, rewrite index page to utilize layouts and partials, edit app name in .env.example

--- laravel-app/resources/views/layouts/app.blade.php
+++ laravel-app/resources/views/layouts/app.blade.php
@@ -5,32 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <title>@yield("title", 'strenght4u')</title>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/carousel-handler.js'])
+        @stack('scripts')  <!-- For additional JS specific to some pages -->
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <body class="bg-primary-grey-100 font-dm">
+        <div class="bg-white px-4 py-4 text-xl font-bold">
+            <a href="{{ route("index") }}">strength4u</a>
         </div>
+        @include('partials.header')  <!-- Always included header -->
+
+        @yield('content')  <!-- Main content varies -->
+
+        @include('partials.footer')  <!-- Always included footer -->
     </body>
 </html>
